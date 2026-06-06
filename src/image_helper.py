8cc82aed81fd2bd088ad58f1908c74f4314f97a0
Sandeep Patnaik <spatnaik359@gmail.com>
2026-06-06 17:51:28 +0530

    added gemini image analysis

---
+++ src/image_helper.py
@@ -0,0 +1,32 @@
+from PIL import Image
+from dotenv import load_dotenv
+import google.generativeai as genai
+import os
+
+load_dotenv()
+
+genai.configure(
+    api_key=os.getenv("GOOGLE_API_KEY")
+)
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+
+def analyze_image(uploaded_file):
+
+    image = Image.open(uploaded_file)
+
+    prompt = """
+    Analyze this image.
+
+    Provide:
+    1. What is visible.
+    2. Important text if any.
+    3. A short summary.
+    """
+
+    response = model.generate_content(
+        [prompt, image]
+    )
+
+    return response.text
--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
 import streamlit as st
 from langchain_helper import get_qa_chain, create_vector_db
+from image_helper import analyze_image
+
 
 st.title(" CUSTOMER SERVICE CHATBOT 🤖")
+
+
+
+
+uploaded_file = st.file_uploader(
+    "Upload Image",
+    type=["jpg", "jpeg", "png"]
+)
+
+
+if uploaded_file:
+
+    image_result = analyze_image(uploaded_file)
+
+    st.subheader("Image Analysis")
+
+    st.write(image_result)
+
+
+
+
 btn = st.button("Create Knowledgebase")
 if btn:
     create_vector_db()
